5ba95f5b342d6602447c151bcd5b8391861ba7a7
Satish Yadav <monstersatish@live.com>
2015-11-03 23:00:46 +0530

    init

---
+++ WinOwin/Auth/AuthMan.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace WinOwin.Auth
+{
+    public class AuthMan
+    {
+        public IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.Current.GetOwinContext().Authentication;
+            }
+        }
+
+        public void IdentitySignIn(string userID)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Email, "s@t.i"));
+            claims.Add(new Claim(ClaimTypes.Name, HttpContext.Current.User.Identity.Name));
+            //var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+            identity.AddClaim(new Claim(ClaimTypes.MobilePhone, "8989481081"));
+            AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = true }, identity);
+        }
+    }
+}
---
+++ WinOwin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using WinOwin.Auth;  
+
+namespace WinOwin.Controllers
+{
+    //[Authorize(Users="Work\\Satish")]
+    //[Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            new AuthMan().IdentitySignIn(Request.GetOwinContext().Authentication.User.Identity.Name);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //var identity = (ClaimsIdentity)User.Identity;
+            var identity = (ClaimsIdentity) Request.GetOwinContext().Authentication.User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            ViewBag.Message = claims.Where(x => x.Type == ClaimTypes.Email).FirstOrDefault().Value;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
